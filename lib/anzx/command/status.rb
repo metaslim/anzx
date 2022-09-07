e0eca709986ccf102e00d651351d3cdd4d3ddcfb
Welly <welly.salim@checkbox.ai>
2022-09-07 15:00:53 +1000

    typo

--- lib/anzx/command/status.rb
+++ lib/anzx/command/status.rb
@@ -11,12 +11,12 @@ module Anzx
 
       def execute(data, input)
         data[:statuses].each_with_index do |status, index|
-          puts "#{index+1}. Control name:\t[#{status.control.name}]
-Asset name:\t\t[#{status.asset_name}]
+          puts "#{index+1}. Control Name:\t[#{status.control.name}]
+Asset Name:\t\t[#{status.asset_name}]
 Status:\t\t\t[#{status.status}]\n\n\n"
         end
       end
-      
+
     end
   end
 end
